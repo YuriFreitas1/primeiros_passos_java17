05fd4bd8e33526aca59194300f6f2fe2ef549ad0
YuriFreitas1 <yuri.freitasdasilva2@gmail.com>
2025-08-10 18:48:55 -0300

    tipos primitivos e testes

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Scream Match");
+        System.out.println("Filme: Megamente");
+
+        String filme = "megamente";
+        int ano = 2010;
+        double notaMegamente = 7.3;
+        boolean disponivelMegamente = true;
+
+        System.out.println("O ano de lançamento de " + filme + " foi em " + ano);
+        System.out.println("Nota do filme: " + notaMegamente);
+        System.out.println("disponibilidade: "+ disponivelMegamente);
+}}
---
+++ src/Teste.java
@@ -0,0 +1,24 @@
+public class Teste {
+    public static void main(String[] args) {
+
+        String filme = "Megamente";
+        String sinopse = "Um vilão azul e com um grandiosa cabeça so usa ela para o mal";
+        float nota = 7.6F;
+        boolean disponivel = true;
+
+        String disponibilidade;
+        if (disponivel) {
+            disponibilidade = "Desponivel";
+        } else {
+            disponibilidade = "Indisponivel";
+        }
+        String descricao = """
+                Filme: %s
+                Sinopse: %s
+                Disponibilidade : %s
+                Nota : %.2f
+                """.formatted(filme, sinopse, disponivel, nota);
+        System.out.println(descricao);
+        //System.out.println(String.format("Filme: %s. disponibilidade: %s. Nota: %.2f",filme,disponibilidade,nota ));
+    }
+}
